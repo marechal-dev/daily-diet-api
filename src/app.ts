bd933b30862a388194cb3dc682da0cdbfea872f8
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-05-03 16:27:24 -0300

    feat: add app-wide routes

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 
 import { appErrorHandler } from './globals/error-handler'
+import { appRoutes } from './routes'
 
 export const app = fastify()
 
 app.register(cookie)
+app.register(appRoutes)
 app.setErrorHandler(appErrorHandler)
